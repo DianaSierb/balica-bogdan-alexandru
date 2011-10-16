c9e4deba4d951ac34a0499a57e4d1b8f6c7ee7f8
bogdan.balica <bogdan.balica@yahoo.com>
2011-10-16 17:37:43 +0000

    Tema

---
+++ Factory/src/Factory.java
@@ -0,0 +1,58 @@
+/*
+a) Testati si rulati acest cod sursa. Explicati ce realizeaza.
+b) Realizati diagrama de clasa pentru codul sursa de mai sus. Explicati relatiile dintre
+clase.
+c) Care este rolul metodei cloan() ?
+d) Ce sablon de proiectare este utilizat in cadrul acestei aplicatii ? Explicati. Utilizati
+pentru aceste explicatii o diagrama de colaborare.
+e) Ce este o clasa incapsulata ( inner class )? Puteti exemplifica pentru cazul de mai sus.
+f) Care este semnificatia modificatorului static pentru atribute, metode si clase ?
+*/
+
+
+public class Factory{
+	interface Xyz {
+		Xyz cloan();
+	}
+	static class Tom implements Xyz {
+		public Xyz cloan() {
+			return new Tom();
+			}
+		public String toString() {
+			return "ttt";
+			}
+		}
+	static class Dick implements Xyz {
+		public Xyz cloan() {
+			return new Dick();
+		    }
+		public String toString() {
+			return "ddd";
+		    }
+		}
+	static class Harry implements Xyz{
+		public Xyz cloan() {
+			return new Harry();
+			}
+		public String toString() {
+			return "hhh";
+		    }
+		}
+	
+	 static class Factory2 {
+		private static java.util.Map<String, Xyz> prototypes=new java.util.HashMap<String, Xyz>();
+		static {
+			prototypes.put("tom", new Tom());
+			prototypes.put("dick", new Dick());
+			prototypes.put("harry", new Harry());
+			}
+		public static Xyz makeObject(String s) {
+			return prototypes.get(s).cloan();
+		}
+		public static void main(String args[]) {
+			for(int i=0;i<args.length;i++) {
+				System.out.println(Factory2.makeObject(args[i])+" ");
+				}
+			}
+		}
+	 }
